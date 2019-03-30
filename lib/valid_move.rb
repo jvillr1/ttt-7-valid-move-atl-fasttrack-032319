dacf61390722e580f38baef0578e47e9f4646be9
Jessica Villarreal <jvillr11@gmail.com>
2019-03-30 20:44:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,9 +4,9 @@ def valid_move?(board, index)
  if index == " " || index == "" || index == nil 
    return false 
  else
-   (position_taken?(board, index)) == false && (board(index) == true)
+  if (position_taken?(board, index)) == false && (board(index) == true)
   return true 
-elsif
+
   return false
 end
 end
